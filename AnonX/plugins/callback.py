a90cee8a0cf9505dd7e5de1e20192ae64601c2a3
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-09 01:06:45 +0300

    Update callback.py

--- AnonX/plugins/callback.py
+++ AnonX/plugins/callback.py
@@ -26,7 +26,6 @@ from AnonX.utils.database import (
 )
 from AnonX.utils.decorators.language import languageCB
 from AnonX.utils.formatters import seconds_to_min
-from AnonX.utils.inline import (
     stream_markup,
     stream_markup_timer,
     telegram_markup,
